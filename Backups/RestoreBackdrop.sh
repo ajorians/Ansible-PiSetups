c896d592024e30be6b44275e31a65035250dc91d
A.J. Orians <ajorians@raspberry400.local>
2024-05-29 22:17:16 -0400

    Fix path.

--- Backups/RestoreBackdrop.sh
+++ Backups/RestoreBackdrop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DATE=`date +%F`
-/home/ajorians/Documents/git/BackupScripts/MediaWiki/RestoreBackddrop.sh /mnt/backups/Mailserver/backdrop/backdrop-$DATE.tar.gz /mnt/backups/Mailserver/backdrop/backdropdb-$DATE.sql
+/home/ajorians/Documents/git/BackupScripts/Backdrop/RestoreBackdrop.sh /mnt/backups/Mailserver/backdrop/backdrop-$DATE.tar.gz /mnt/backups/Mailserver/backdrop/backdropdb-$DATE.sql
