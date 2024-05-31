f925101538c9497a35b55926a6737d54dec07f46
A.J. Orians <ajorians@raspberry400.local>
2024-05-30 22:10:50 -0400

    Update Restore file to use latest file.

--- Backups/RestoreMediaWiki.sh
+++ Backups/RestoreMediaWiki.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-DATE=`date +%F`
-/home/ajorians/Documents/git/BackupScripts/MediaWiki/RestoreMediaWiki.sh /mnt/backups/Mailserver/mediawiki/mediawiki-$DATE.tar.gz /mnt/backups/Mailserver/mediawiki/mediawikidb-$DATE.sql
+#Get latest file
+LATESTFILE=`find /mnt/backups/Mailserver/mediawiki/ -maxdepth 1 -name 'mediawiki-*.tar.gz' -printf '%f\n' | sort -nr | head -n1`
+
+echo "LatestFile: $LATESTFILE"
+
+LATESTDATE=`grep -oP "\d{4}-\d{2}-\d{2}" <<< "$LATESTFILE"`
+
+echo "LatestDate: $LATESTDATE"
+
+/home/ajorians/Documents/git/BackupScripts/MediaWiki/RestoreMediaWiki.sh /mnt/backups/Mailserver/mediawiki/mediawiki-$LATESTDATE.tar.gz /mnt/backups/Mailserver/mediawiki/mediawikidb-$LATESTDATE.sql
