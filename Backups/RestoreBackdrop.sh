6ec8f7f5ce328ca14c82a30f2fad7eaf7eea0f69
A.J. Orians <ajorians@raspberry400.local>
2024-05-30 22:14:46 -0400

    Use latest files in restore script.

--- Backups/RestoreBackdrop.sh
+++ Backups/RestoreBackdrop.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-DATE=`date +%F`
-/home/ajorians/Documents/git/BackupScripts/Backdrop/RestoreBackdrop.sh /mnt/backups/Mailserver/backdrop/backdrop-$DATE.tar.gz /mnt/backups/Mailserver/backdrop/backdropdb-$DATE.sql
+#Get latest file
+LATESTFILE=`find /mnt/backups/Mailserver/backdrop/ -maxdepth 1 -name 'backdrop-*.tar.gz' -printf '%f\n' | sort -nr | head -n1`
+
+echo "LatestFile: $LATESTFILE"
+
+LATESTDATE=`grep -oP "\d{4}-\d{2}-\d{2}" <<< "$LATESTFILE"`
+
+echo "LatestDate: $LATESTDATE"
+
+/home/ajorians/Documents/git/BackupScripts/Backdrop/RestoreBackdrop.sh /mnt/backups/Mailserver/backdrop/backdrop-$LATESTDATE.tar.gz /mnt/backups/Mailserver/backdrop/backdropdb-$LATESTDATE.sql
